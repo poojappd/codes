a7444e174fc6ea3d4d4a2d80f053295371b96098
poojappd <48831727+poojappd@users.noreply.github.com>
2019-03-24 13:15:03 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,9 @@
+a=int(input("enter num"))
+b=int(input("enter num"))
+c=int(input("enter num"))
+if(a>b and a>c):
+  print("%s is greater"%a)
+elif(b>c):
+  print("%s is greater"%b)
+else:
+  print("s is greater"%c)
